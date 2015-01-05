a5255a08c64d96fc54cf3811c24f6aceae318868
samuelnastavisual <samuel.nasta.visual@gmail.com>
2015-01-05 17:54:11 -0200

    Init

---
+++ app/scripts/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+$(document).ready(function(){
+//	$.ajax({
+//		type: 'GET',
+//		url: '../situacao.xml',
+//		dataType: 'xml',
+//		success: function(xml) {
+//			$(xml).find('site').each(function(){
+//				var id = $(this).attr('id');
+//				var title = $(this).find('title').text();
+//				var url = $(this).find('url').text();
+//				$('<div class="items" id="link_'+id+'"></div>').html('<a href="'+url+'">'+title+'</a>').appendTo('#page-wrap');
+//				$(this).find('desc').each(function(){
+//					var brief = $(this).find('brief').text();
+//					var long = $(this).find('long').text();
+//					$('<div class="brief"></div>').html(brief).appendTo('#link_'+id);
+//					$('<div class="long"></div>').html(long).appendTo('#link_'+id);
+//				});
+//			});
+//		}
+//	});
+    $('.modal').on('click', function() {
+        $('body').toggleClass('dialogIsOpen');
+    });
+});
